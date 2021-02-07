ae6b67de848aeb542e00a5484a830cdc753a3e2e
jasper <jasperzhou1129@gmail.com>
2021-02-07 15:12:44 -0500

    added codmments

--- KMP/main.cpp
+++ KMP/main.cpp
@@ -11,6 +11,11 @@ using namespace std;
 class Solution {
 public:
     int strStr(string haystack, string needle) {
+        /*
+         haystack is pattern string
+         needle is target string
+         */
+        
         int n = haystack.size();
         int m = needle.size();
         if(m == 0) {
@@ -24,6 +29,7 @@ public:
         
         dp[0] = haystack[0] == needle[0];
         if(m == 1 && dp[0]) return 0;
+        // iterate over haystack, try to match haystack's current suffix with prefix of needle
         for(int i = 1; i < n; i++) {
             int j = dp[i-1];
             while(j >= 1 && haystack[i] != needle[j]) {
@@ -36,7 +42,11 @@ public:
         }
         return -1;
     }
-    
+    i - x + 1 = k
+    /*
+     max len such that needle[0:k-1] = needle[i-k+1:i]
+     This is used to calculate needle's suffix
+     */
     vector<int> getSuffix(string& needle) {
         int n = needle.size();
         vector<int> dp(n,0);
